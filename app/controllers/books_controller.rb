cca504bd7064db6dc9f56874ac98678d558ad9dc
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-03-21 22:38:56 +0100

    Created endpoint for creating book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,23 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+    book_create_status(book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+
+  def book_create_status(book)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
 end
