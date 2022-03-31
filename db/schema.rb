2f11293d628081ffd5a3eeb8a1e7fd11116abb24
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-03-31 15:23:30 +0100

    Create author model and association with book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_160956) do
+ActiveRecord::Schema.define(version: 2022_03_30_230942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+  create_table "authors", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
